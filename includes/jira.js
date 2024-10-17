16480a026892950e43fdf8e5d1d000aa210198d5
jeremy.rochot@externe.systeme-u.fr <jeremy.rochot@externe.systeme-u.fr>
2024-10-17 12:05:39 +0000

    Refonte

---
+++ includes/jira.js
@@ -0,0 +1,8 @@
+const data_source =  "`tec-pocappscript-r-6lbj.JIRA_REWORK.00_data_source`";
+const mapping_etat = "`tec-pocappscript-r-6lbj.JIRA_REWORK.00_mapping_etat`";
+
+
+module.exports = {  
+    data_source,
+    mapping_etat
+};
