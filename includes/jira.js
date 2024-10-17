a7bf1dd16094f1003568634b46101a72c28b4290
ameni.akkeri@externe.systeme-u.fr <ameni.akkeri@externe.systeme-u.fr>
2024-10-17 14:58:38 +0000

    nettoyage

--- includes/jira.js
+++ includes/jira.js
@@ -1,8 +1,17 @@
 const data_source =  "`tec-pocappscript-r-6lbj.JIRA_REWORK.00_data_source`";
 const mapping_etat = "`tec-pocappscript-r-6lbj.JIRA_REWORK.00_mapping_etat`";
+const mapping_regroupement = "`tec-pocappscript-r-6lbj.JIRA_REWORK.00_mapping_regroupement`";
+const mapping_type = "`tec-pocappscript-r-6lbj.JIRA_REWORK.00_mapping_type`";
+const mapping_dev = "`tec-pocappscript-r-6lbj.JIRA_REWORK.00_mapping_dev`";
+
+
+
 
 
 module.exports = {  
     data_source,
-    mapping_etat
+    mapping_etat,
+    mapping_regroupement,
+    mapping_type,
+    mapping_dev
 };
