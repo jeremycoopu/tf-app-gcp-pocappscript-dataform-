554504b07a8391030e2fe47041972fb479c72ff4
ameni.akkeri@externe.systeme-u.fr <ameni.akkeri@externe.systeme-u.fr>
2024-10-21 13:40:51 +0000

    all columns except  AK

--- includes/jira.js
+++ includes/jira.js
@@ -3,6 +3,12 @@ const mapping_etat = "`tec-pocappscript-r-6lbj.JIRA_REWORK.00_mapping_etat`";
 const mapping_regroupement = "`tec-pocappscript-r-6lbj.JIRA_REWORK.00_mapping_regroupement`";
 const mapping_type = "`tec-pocappscript-r-6lbj.JIRA_REWORK.00_mapping_type`";
 const mapping_dev = "`tec-pocappscript-r-6lbj.JIRA_REWORK.00_mapping_dev`";
+const mapping_resolution = "`tec-pocappscript-r-6lbj.JIRA_REWORK.00_mapping_resolution`";
+const calendrier = "`tec-pocappscript-r-6lbj.JIRA_REWORK.00_calendrier`";
+const source_jira_feature_vide = "`tec-pocappscript-r-6lbj.JIRA_REWORK.00_source_jira_vide`";
+
+
+
 
 
 
@@ -13,5 +19,8 @@ module.exports = {
     mapping_etat,
     mapping_regroupement,
     mapping_type,
-    mapping_dev
+    mapping_dev,
+    mapping_resolution,
+    calendrier,
+   source_jira_feature_vide 
 };
